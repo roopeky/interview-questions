26dc154cdff998c3a94c82e946a440fb74181803
roopeky <91128629+roopeky@users.noreply.github.com>
2023-04-27 09:45:42 +0300

    Add files via upload

---
+++ fourth/Main.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        Map<Integer, Integer> verticalSums = verticalSum(root);
+
+        for (int hd : verticalSums.keySet()) {
+            System.out.println("Vertical sum at hd=" + hd + ": " + verticalSums.get(hd));
+        }
+    }
+
+    public static Map<Integer, Integer> verticalSum(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        verticalSumHelper(root, map, 0);
+        return map;
+    }
+
+    private static void verticalSumHelper(TreeNode root, Map<Integer, Integer> map, int hd) {
+        if (root == null) {
+            return;
+        }
+
+        int sum = map.getOrDefault(hd, 0);
+        map.put(hd, sum + root.val);
+
+        verticalSumHelper(root.left, map, hd - 1);
+        verticalSumHelper(root.right, map, hd + 1);
+    }
+}
