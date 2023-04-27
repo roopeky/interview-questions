70ffc87fd312da16b6bb3d8e81ae0cce51f74a2e
roopeky <91128629+roopeky@users.noreply.github.com>
2023-04-27 09:36:26 +0300

    Add files via upload

---
+++ first/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TreeNode p = new TreeNode(3);
+        p.left = new TreeNode(4);
+        p.right = new TreeNode(5);
+        p.left.left = new TreeNode(1);
+        p.left.right = new TreeNode(2);
+
+        TreeNode q = new TreeNode(4);
+        q.left = new TreeNode(1);
+        q.right = new TreeNode(2);
+
+        boolean isSubtree = isSubtree(p, q);
+
+        System.out.println(isSubtree);
+    }
+
+    public static boolean isSubtree(TreeNode p, TreeNode q) {
+        if (p == null) {
+            return false;
+        }
+        if (q == null) {
+            return true;
+        }
+        if (isIdentical(p, q)) {
+            return true;
+        }
+        return isSubtree(p.left, q) || isSubtree(p.right, q);
+    }
+
+    public static boolean isIdentical(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isIdentical(p.left, q.left) && isIdentical(p.right, q.right);
+    }
+}
