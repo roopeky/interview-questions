c1c9045604d82ebbd8e2240131dcb57fff6cd52c
roopeky <91128629+roopeky@users.noreply.github.com>
2023-04-27 10:35:40 +0300

    Update Main.java

--- first/Main.java
+++ first/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        // sub-tree exercise
         TreeNode p = new TreeNode(3);
         p.left = new TreeNode(4);
         p.right = new TreeNode(5);
